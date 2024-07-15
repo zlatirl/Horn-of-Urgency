525c36e5968bf9be3a976b5b2620fe145a1cdbfe
unknown <zlatirl1@gmail.com>
2024-07-15 06:27:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Horn of Urgency
+
+This project includes a Discord bot that inputs 4 randomly generated NMs (Notorious Monsters) into the chat from a user-inputted command.
+
+## Installation
+
+1. Clone the repository.
+2. Install dependencies with `npm install`.
+3. Set up your environment variables in the `.env` file.
+
+## Structure
+
+```bash
+├── .gitignore
+├── package.json
+├── .env
+└── src
+   ├── index.js
+   └── names.json
+```
+
+## Usage
+
+Run the bot with `node .`.
+
+## License
+
+[MIT License](LICENSE)
---
+++ src/index.js
@@ -0,0 +1,62 @@
+// Import the modules we need
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits, EmbedBuilder, SlashCommandBuilder, PermissionsBitField, Permissions } = require('discord.js');
+
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+// Construct the full path to names.json within the src folder
+const namesFilePath = path.join(__dirname, 'names.json');
+
+// Read the names from the JSON file
+const rawNames = fs.readFileSync(namesFilePath);
+const namesData = JSON.parse(rawNames);
+const namesArray = namesData.names;
+
+// ID of the specific channel where the command should be allowed
+const allowedChannelId = process.env.CHANNEL_ID;
+
+// Event listener for when the bot is ready
+client.on("ready", (c) => {
+    console.log(`${c.user.tag} is ready!`);
+
+    // Define the /roulette command
+    const roulette = new SlashCommandBuilder()
+    .setName('roulette')
+    .setDescription('Randomly select 4 NMs')
+
+    // Register the command
+    client.application.commands.create(roulette); 
+});
+
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    // Check if the interaction happened in the allowed channel
+    const { channelId } = interaction;
+    if (channelId !== allowedChannelId) {
+        interaction.reply( { content: 'This command can only be used in a specific channel.', ephemeral: true });
+        return;
+    }
+
+    // Shuffle the array to ensure randomness
+    const shuffledNames = namesArray.sort(() => Math.random() - 0.5);
+
+    // Get the first 4 names
+    const selectedNames = shuffledNames.slice(0, 4);
+
+    // Respond with the selected names
+    interaction.reply(`NMs of the week: ${selectedNames.join(', ')}`);
+});
+
+// Login to Discord with the bot token from the .env file
+client.login(process.env.BOT_TOKEN);
